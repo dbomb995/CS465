29919911341714c2243863b918d4a2045a0364b0
Derek Bomboy <dbomb995@gmail.com>
2020-09-20 14:28:24 -0600

    Adding module 3 changes to new branch

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+var hbs = require('hbs')
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
-const travelRouter = require('/app_server/routes/travel');
+const travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 //register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(_dirname, 'app_server', 'views/partials'));
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.set('view engine', 'hbs');
 
