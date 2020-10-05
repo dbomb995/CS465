e577d01994f612e3410fa69b395aead194761082
Derek Bomboy <dbomb995@gmail.com>
2020-10-04 19:24:17 -0600

    Mod 5 changes

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
-const host = process.env.DB_HOST || "127.0.0.1";
-const dbURI = "mongodb://${host}/travlr";
-const readLine = require("readline");
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1';
+const dbURI = `mongodb://${host}/travlr`;
+const readLine = require('readline');
 
 mongoose.set("useUnifiedTopology", true);
 
 const connect = () => {
   setTimeout(
     () =>
+ 
       mongoose.connect(dbURI, {
         useNewUrlParser: true,
         useCreateIndex: true,
@@ -23,6 +24,7 @@ mongoose.connection.on("error", (err) => {
   console.log("Mongoose connection error:", err);
 });
 mongoose.connection.on("disconnected", () => {
+  console.log(dbURI);
   console.log("Mongoose disconnected");
 });
 
